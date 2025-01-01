176586fedab88bac7f6ec4b766a58fefe34df145
iman <dev.imansabet@gmail.com>
2025-01-01 21:08:26 +0330

    Add AppAction constants and AppPermission record for managing user permissions

---
+++ Constants/Authentication/AppPermission.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthLibrary.Constants.Authentication;
+
+public record AppPermission
+    (string Service,string Feature
+    ,string Action,string Group
+    ,string Description,bool IsBasic = false
+    )
+{
+    public string Name => NameFor(Service, Feature, Action);
+    public static string NameFor(string service, string feature, string action) 
+    {
+        return $"Permission. {service}.{feature}.{action}";
+    }
+
+}
+ 
