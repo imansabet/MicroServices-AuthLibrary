173084714c8ff2d91297aebde5c92b0aeebc0a34
iman <dev.imansabet@gmail.com>
2025-01-03 21:35:41 +0330

    Add MustHavePermissionAttribute for simplified authorization policy application in controllers

---
+++ Attributes/MustHavePermissionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using AuthLibrary.Constants.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthLibrary.Attributes;
+
+public class MustHavePermissionAttribute : AuthorizeAttribute
+{
+    public MustHavePermissionAttribute(string service , string feature,string action)
+    {
+        Policy = AppPermission.NameFor(service, feature, action);
+    }
+}
