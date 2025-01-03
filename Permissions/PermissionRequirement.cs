03248ada65956d46bfffe1cc82252f2cdb84aa4d
iman <dev.imansabet@gmail.com>
2025-01-03 17:05:15 +0330

    Add PermissionRequirement class for custom authorization based on permissions

---
+++ Permissions/PermissionRequirement.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AuthLibrary.Permissions;
+
+public class PermissionRequirement : IAuthorizationRequirement
+{
+    public string Permission { get; set; }
+    public PermissionRequirement(string permission)
+    {
+        Permission = permission;
+    }
+}
