65edcd054cd99f20324e19298b075c961064a6f4
iman <dev.imansabet@gmail.com>
2025-01-03 17:42:32 +0330

    Implement PermissionPolicyProvider for dynamic authorization policy resolution

---
+++ Permissions/PermissionPolicyProvider.cs
@@ -0,0 +1,34 @@
+﻿using AuthLibrary.Constants.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace AuthLibrary.Permissions;
+
+public class PermissionPolicyProvider : IAuthorizationPolicyProvider
+{
+    public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get;}
+    public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+    {
+        FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+    }
+    public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+    {
+        return FallbackPolicyProvider.GetDefaultPolicyAsync();
+    }
+
+    public Task<AuthorizationPolicy> GetFallbackPolicyAsync()
+    {
+        return Task.FromResult<AuthorizationPolicy>(null);
+    }
+
+    public Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+    {
+        if(policyName.StartsWith(AppClaim.Permission , StringComparison.CurrentCultureIgnoreCase)) 
+        {
+            var policy = new AuthorizationPolicyBuilder();
+            policy.AddRequirements(new PermissionRequirement(policyName));
+            return Task.FromResult(policy.Build());
+        }
+        return FallbackPolicyProvider.GetPolicyAsync(policyName);
+    }
+}
