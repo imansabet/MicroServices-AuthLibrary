b4af100a89f9ddc5be9a33225a61ca7906cc44d2
iman <dev.imansabet@gmail.com>
2025-01-03 16:52:53 +0330

    Define constants for authentication claims (Permission, Expiration, Issuer)

---
+++ Constants/Authentication/AppClaim.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthLibrary.Constants.Authentication;
+
+public static class AppClaim
+{
+    public const string Permission = "permission";
+    public const string Expiration = "exp";
+    public const string Issuer = "gateway";
+
+
+}
