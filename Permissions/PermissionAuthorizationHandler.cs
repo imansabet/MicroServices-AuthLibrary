d11f377cb896e24c5130545dd77c9aad197fa402
iman <dev.imansabet@gmail.com>
2025-01-03 18:20:30 +0330

    Add PermissionAuthorizationHandler for validating user permissions in authorization process

---
+++ Permissions/PermissionAuthorizationHandler.cs
@@ -0,0 +1,30 @@
+﻿using AuthLibrary.Constants.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthLibrary.Permissions;
+
+public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
+{
+    public PermissionAuthorizationHandler()
+    {
+        
+    }
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+    {
+        if(context.User is null) 
+        {
+            await Task.CompletedTask;
+        }
+        var permissions = context.User.Claims
+            .Where
+            ( claim => claim.Type == AppClaim.Permission 
+              && claim.Value == requirement.Permission 
+              && claim.Issuer == AppClaim.Issuer);
+        
+        if (permissions.Any()) 
+        {
+            context.Succeed(requirement);
+            await Task.CompletedTask;
+        }
+    }
+}
